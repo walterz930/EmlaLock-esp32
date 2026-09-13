af47d52d2080fbbab67fed32d058e8bf4f9746fe
walterz930 <120287113+walterz930@users.noreply.github.com>
2026-09-13 23:12:58 +0100

    Release v1.0.6

--- src/update_manager.h
+++ src/update_manager.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #ifndef EMLALOCK_VERSION
-#define EMLALOCK_VERSION "1.0.5"
+#define EMLALOCK_VERSION "1.0.6"
 #endif
 void initUpdateManager();
 void handleUpdateCheck();
